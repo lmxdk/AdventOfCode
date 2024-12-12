5de0e278b8b865e12f6cf31e3e6de860f0965406
Lau Maack-Krommes <Lau.Maack-Krommes@keylane.com>
2024-12-12 22:21:00 +0100

    Day 17 Part 2

--- AdventOfCodeCS/2015/Day17.cs
+++ AdventOfCodeCS/2015/Day17.cs
@@ -12,23 +12,35 @@ namespace AdventOfCodeCS._2015
             var input = new [] { 43,3,4,10,21,44,4,6,47,41,34,17,17,44,36,31,46,9,27,38 };
             var goal = 150;
             var permutations = 0;
-            
+            var minContainerCount = int.MaxValue;
+
             var max = Math.Pow(2, input.Length);
             for (var iteration = 2; iteration < max; iteration++)
             {
                 var sum = 0;
+                var containerCount = 0;
                 for (var i = 0; i < input.Length; i++)
                 {
                     var mask = 1 << i;
                     if ((iteration & mask) == mask)
                     {
+                        containerCount++;
                         sum += input[i];
                     }
                 }
 
                 if (sum == goal)
                 {
-                    permutations++;
+                    if (containerCount < minContainerCount)
+                    {
+                        permutations = 0;
+                        minContainerCount = containerCount;
+                    }
+
+                    if (containerCount == minContainerCount)
+                    {
+                        permutations++;
+                    }
                 }
             }
             
